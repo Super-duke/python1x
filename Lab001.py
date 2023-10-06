d94143705c3b1bc639e93512d6dafee3585bf4f4
Super-duke <38130431+Super-duke@users.noreply.github.com>
2023-10-06 13:34:02 +0530

    Add files via upload

---
+++ Lab001.py
@@ -0,0 +1,9 @@
+# Dynamic First name and last name
+#name1 = input("Enter your First Name")
+#name2 = input("Enter your Last Name")
+#print(name1,name2,sep='.',end='/t')
+
+
+#dynamic name with welcome message
+name = input("Enter your name")
+print("You are welcome", name, "to the class")
